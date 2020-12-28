9e9dd71c333b95f60ed25d37f1a8f14b3c362c4b
creattech <crystalsmcneil@gmail.com>
2020-12-28 14:51:23 -0600

    refactor findNemo

--- big-o.js
+++ big-o.js
@@ -1,16 +1,23 @@
 let nemo = ['nemo'];
 let notNemo = ['Sharkbert'];
+let everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
 
 const findNemo = (arr) => {
     let t0 = performance.now();
     console.log(t0);
-    for (let arrElement of arr) {
-        return arrElement === 'nemo' ? 'Found Nemo' : 'Nemo not Found';
+    for (let item of arr) {
+        if (arr.includes('nemo')) {
+            return 'Nemo found';
+        } else {
+            return 'Nemo not found';
+        }
+
     }
 }
 
 console.log(findNemo(nemo));
 console.log(findNemo(notNemo));
+console.log(findNemo(everyone));
 
 
 
